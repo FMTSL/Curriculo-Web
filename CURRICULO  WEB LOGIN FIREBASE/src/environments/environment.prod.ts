c399ae380d0e4dd08574b392f8705c324e0740f1
Felipe Matos <Felipe_Matos@hotmail.com.br>
2022-01-19 18:31:50 -0300

    Cadastro e Login no Firebase

---
+++ CURRICULO  WEB LOGIN FIREBASE/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'curriculoweb-6f95c',
+    appId: '1:677177054846:web:ba2498328137ebd2952a2f',
+    storageBucket: 'curriculoweb-6f95c.appspot.com',
+    apiKey: 'AIzaSyAFlaJ9kID7fXHr9A3ukHGFt_CWc5OAa2E',
+    authDomain: 'curriculoweb-6f95c.firebaseapp.com',
+    messagingSenderId: '677177054846',
+  },
+  production: true
+};
---
+++ CURRICULO  WEB LOGIN FIREBASE/src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'curriculoweb-6f95c',
+    appId: '1:677177054846:web:ba2498328137ebd2952a2f',
+    storageBucket: 'curriculoweb-6f95c.appspot.com',
+    apiKey: 'AIzaSyAFlaJ9kID7fXHr9A3ukHGFt_CWc5OAa2E',
+    authDomain: 'curriculoweb-6f95c.firebaseapp.com',
+    messagingSenderId: '677177054846',
+  },
+  production: false,
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
